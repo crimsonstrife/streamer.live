05b41b3b2ffa0374e51cb5220e7574458f69c2b4
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2025-01-27 11:02:11 -0500

    Add Spatie Laravel Permission package and implement role management

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['Admin', 'Moderator', 'Editor', 'Viewer'];
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+    }
+}
