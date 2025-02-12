fc108507510350aa24726c2c9a139f27b26bea34
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2025-02-12 12:45:37 -0500

    feat: add colors.css to Vite configuration for improved styling

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/colors.css',
                 'resources/css/filament/admin/theme.css',
                 'resources/js/app.js',
                 'tailwind.config.js'],
