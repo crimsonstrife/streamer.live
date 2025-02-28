473e991259cf81606129fd93059d7e1133aa6900
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2025-02-28 13:16:12 -0500

    feat: add TwitchEmbed block for Filament with customizable options

---
+++ app/Filament/Fabricator/PageBlocks/TwitchEmbed.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks;
+
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class TwitchEmbed extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('twitch-embed')
+            ->schema([
+                TextInput::make('channel')
+                    ->label('Channel')
+                    ->placeholder('Channel name')
+                    ->required(),
+                Toggle::make('chat')
+                    ->label('Show chat')
+                    ->default(false),
+                Toggle::make('autoplay')
+                    ->label('Autoplay')
+                    ->default(false),
+            ]);
+    }
+
+    public static function mutateData(array $data): array
+    {
+        return $data;
+    }
+}
---
+++ resources/views/components/filament-fabricator/page-blocks/twitch-embed.blade.php
@@ -0,0 +1,15 @@
+@aware(['page'])
+<div class="px-4 py-4 md:py-8">
+    <div class="mx-auto max-w-7xl">
+        <div class="aspect-w-16 aspect-h-9">
+            <iframe src="https://embed.twitch.tv/?allowfullscreen=true&channel={{ $channel }}&layout=video&parent={{ request()->getHost() }}" height="400" width="100%" allow="autoplay; fullscreen" scrolling="no" allowfullscreen
+                sandbox="allow-modals allow-scripts allow-same-origin allow-popups allow popups-to-escape-sandbox allow-storage-access-by-user-activation" id="stream_embed">
+            </iframe>
+            @if ($chat)
+                <iframe src="https://www.twitch.tv/embed/{{ $channel }}/chat?parent={{ request()->getHost() }}"
+                    height="300" width="100%" id="chat_embed">
+                </iframe>
+            @endif
+        </div>
+    </div>
+</div>
