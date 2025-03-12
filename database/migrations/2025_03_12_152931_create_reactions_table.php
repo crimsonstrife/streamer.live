495d94f7c2e614606d38515b41956cb16ab8a446
Patrick Barnhardt <pbarnhardt@charlottepipe.com>
2025-03-12 16:07:28 -0400

    feat: add migrations for reactions, comments, and guests tables

---
+++ database/migrations/2025_03_12_152931_create_reactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\ReactionType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('owner'); // Who reacted
+            $table->morphs('reactable'); // The model that was reacted to (e.g. Comment, Reply)
+            $table->enum('type', ReactionType::getValues()); // The type of reaction (e.g. like, dislike)
+            $table->integer('point_value')->default(1); // The value of the reaction (e.g. 1 for like, -1 for dislike)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
---
+++ database/migrations/2025_03_12_175318_create_guests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('ip_address')->nullable();
+            $table->string('user_agent')->nullable();
+            $table->boolean('is_spammer')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
