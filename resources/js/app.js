c914d68c1e7527851a63858154996a4d5a3397da
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-03-25 12:32:08 -0400

    Add Swiper library integration for enhanced UI functionality
    Integrated Swiper library and its modules (Navigation, Thumbs) into the project. Updated CSS imports and JavaScript setup to enable global usage of Swiper and its features. This paves the way for implementing advanced carousel and navigation components.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,17 @@
 import $ from 'jquery'
 import * as bootstrap from 'bootstrap'
+import Swiper from 'swiper'
+import { Navigation, Thumbs } from 'swiper/modules'
+
+Swiper.use([Navigation, Thumbs]);
 
 // Use Bootstrap globally
 window.bootstrap = bootstrap;
 
 // Use jQuery globally
 window.$ = window.jQuery = $
+
+// Use Swiper globally
+window.Swiper = Swiper
+window.SwiperModules = { Navigation, Thumbs };
+
