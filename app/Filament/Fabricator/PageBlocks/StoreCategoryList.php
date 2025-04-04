06baf965982b545e0bfe33f0a1042ee24bd5d72b
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-04-03 20:45:34 -0400

    Add store category list block for Filament Fabricator
    Introduced a new page block component to display a list of store categories. The block includes a customizable title and dynamically fetches categories along with their product counts for display.

---
+++ app/Filament/Fabricator/PageBlocks/StoreCategoryList.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks;
+
+use App\Models\Category;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\TextInput;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class StoreCategoryList extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('store-category-list')
+            ->schema([
+                TextInput::make('title')->label('Section Title')->required()->default('Shop by Category'),
+            ]);
+    }
+
+    public static function mutateData(array $data): array
+    {
+        return [
+            'title' => $data['title'] ?? 'Shop by Category',
+            'categories' => Category::where('type', 'product')->withCount('products')->orderBy('name')->get(),
+        ];
+    }
+}
