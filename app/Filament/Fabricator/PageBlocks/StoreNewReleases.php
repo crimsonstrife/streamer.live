83317af5483378712fe0037780adcc51b085ec6b
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-04-03 20:17:19 -0400

    Add "Store New Releases" page block
    Introduce a new Filament Fabricator page block to display recent product releases. The block allows configuration of the section title and the number of products to show, pulling the latest products dynamically. Includes a corresponding Blade view to render the block with structured styling and product details.

---
+++ app/Filament/Fabricator/PageBlocks/StoreNewReleases.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks;
+
+use App\Models\Product;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\TextInput;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class StoreNewReleases extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('store-new-releases')
+            ->schema([
+                TextInput::make('title')->label('Section Title')->default('New Releases')->required(),
+                TextInput::make('product_count')->label('Number of Products to show')->numeric()->default(4)->required(),
+            ]);
+    }
+
+    public static function mutateData(array $data): array
+    {
+        return [
+            'title' => $data['title'] ?? 'New Releases',
+            'products' => Product::latest()->take($data['product_count'])->get(),
+        ];
+    }
+}
