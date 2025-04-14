95779afea93f54dd1d1d2cb5e6d8c8d528895e45
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-04-14 14:08:57 -0400

    Update `type` column in pages table migration
    Replaced `type` column definition with an enum containing specific values. This ensures stricter data consistency and aligns with predefined page types. The change is backward-compatible as the column remains nullable.

--- database/migrations/2025_04_08_022058_add_type_to_pages_table.php
+++ database/migrations/2025_04_08_022058_add_type_to_pages_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('pages', function (Blueprint $table) {
-            $table->string('type')->nullable()->after('layout');
+            $table->enum('type', ['shop', 'product_detail', 'collection_detail', 'blog', 'blog_detail'])->nullable()->after('layout');
         });
     }
 
