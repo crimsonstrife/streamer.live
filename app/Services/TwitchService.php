d77bf700cccc552c4bb9282df47e8c66b134888f
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-04-29 15:46:52 -0400

    Refactor TwitchService to use TwitchSettings configuration
    Replaced direct configuration calls with dependency injection of TwitchSettings. Improved code readability, replaced private variables with public properties where appropriate, and added default logic for channel name and SSL verification options. This ensures better flexibility and centralized configuration management.

--- app/Services/TwitchService.php
+++ app/Services/TwitchService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Settings\TwitchSettings;
 use Exception;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
@@ -11,23 +12,28 @@ use Throwable;
 
 class TwitchService
 {
-    private string $clientId;
+    protected bool $enabled;
 
-    private string $clientSecret;
+    public ?string $channel_name;
 
-    private string $accessToken;
+    public ?string $client_id;
+
+    public ?string $client_secret;
+
+    private ?string $access_token;
+
+    public bool $ssl_verify;
 
     /**
      * @throws Exception
      */
     public function __construct()
     {
-        if (! config('services.twitch.enabled', false)) {
-            return;
-        }
-
-        $this->clientId = config('services.twitch.client_id');
-        $this->clientSecret = config('services.twitch.client_secret');
+        $this->enabled = app(TwitchSettings::class)->enabled;
+        $this->channel_name = app(TwitchSettings::class)->channel_name;
+        $this->client_id = app(TwitchSettings::class)->client_id;
+        $this->client_secret = app(TwitchSettings::class)->client_secret;
+        $this->ssl_verify = app(TwitchSettings::class)->ssl_verify;
         $this->authenticate();
     }
 
@@ -38,15 +44,21 @@ class TwitchService
     private function authenticate(): void
     {
         // Check if Twitch credentials are configured
-        if (empty($this->clientId) || empty($this->clientSecret) || $this->clientId === 'your-client-id' || $this->clientSecret === 'your-client-secret') {
+        if (empty($this->client_id) || empty($this->client_secret) || $this->client_id === 'your-client-id' || $this->client_secret === 'your-client-secret') {
             Log::warning('Twitch authentication skipped: Missing client ID or secret.');
 
             return; // Prevent further execution
         }
 
-        $response = Http::withOptions(['verify' => config('services.twitch.verify')])->post('https://id.twitch.tv/oauth2/token', [
-            'client_id' => $this->clientId,
-            'client_secret' => $this->clientSecret,
+        if (empty($this->channel_name)) {
+            Log::warning('Twitch authentication skipped: Missing channel or username.');
+
+            return; // Prevent further execution
+        }
+
+        $response = Http::withOptions(['verify' => $this->ssl_verify])->post('https://id.twitch.tv/oauth2/token', [
+            'client_id' => $this->client_id,
+            'client_secret' => $this->client_secret,
             'grant_type' => 'client_credentials',
         ]);
 
@@ -56,22 +68,26 @@ class TwitchService
             throw new RuntimeException('Failed to retrieve Twitch access token: '.json_encode($json, JSON_THROW_ON_ERROR));
         }
 
-        $this->accessToken = $json['access_token'];
+        $this->access_token = $json['access_token'];
     }
 
-    public function getStreamData($username)
+    public function getStreamData($username = null)
     {
+        if (empty($username)) {
+            $username = $this->channel_name;
+        }
+
         $username = strtolower(trim($username));
 
         Log::debug("TwitchService: Fetching stream data for user: {$username}");
 
         try {
             $response = Http::withOptions([
-                'verify' => config('services.twitch.verify'),
+                'verify' => $this->ssl_verify,
             ])
                 ->withHeaders([
-                    'Client-ID' => $this->clientId,
-                    'Authorization' => 'Bearer '.$this->accessToken,
+                    'Client-ID' => $this->client_id,
+                    'Authorization' => 'Bearer '.$this->access_token,
                 ])
                 ->get('https://api.twitch.tv/helix/streams', [
                     'user_login' => $username,
