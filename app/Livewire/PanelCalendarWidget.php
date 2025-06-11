b64db5260dc53d8c80eaf12e6fe67a59daa13f8f
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-06-11 14:54:54 -0400

    Refactor event handling for calendar widget integration
    Updated `PanelCalendarWidget` and `Event` model to enhance compatibility with the calendar system. Refactored methods to utilize timezone handling, resource management, and extended properties. Introduced logging in `getEvents` and transitioned to more robust object mapping for calendar events.

--- app/Livewire/PanelCalendarWidget.php
+++ app/Livewire/PanelCalendarWidget.php
@@ -5,9 +5,22 @@ namespace App\Livewire;
 use App\Models\Event;
 use Guava\Calendar\Widgets\CalendarWidget;
 use Guava\Calendar\ValueObjects\CalendarEvent;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 
 class PanelCalendarWidget extends CalendarWidget
 {
-    protected static string $view = 'livewire.panel-calendar-widget';
+    protected static string $view = 'guava-calendar::widgets.calendar';
+
+    //protected string $calendarView = 'resourceTimeGridWeek';
+
+    public function getEvents(array $fetchInfo = []): Collection | array
+    {
+        $calendarEvents = Event::all()
+            ->map(fn ($event) => $event->toCalendarEvent())->toArray();
+
+        Log::debug('Events Array: ' . print_r($calendarEvents, true));
+
+        return $calendarEvents;
+    }
 }
