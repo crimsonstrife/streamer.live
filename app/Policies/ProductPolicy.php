c3e0e5684517a174588b5df6071f7f77c5922fa9
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-07-11 12:58:21 -0400

    Add initial policy classes for Product, Collection, and Order
    Introduce `ProductPolicy`, `CollectionPolicy`, and `OrderPolicy` to define authorization logic. These classes will serve as a foundation for implementing access control for their respective models.

---
+++ app/Policies/ProductPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\AuthObjects\User;
+
+class ProductPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
