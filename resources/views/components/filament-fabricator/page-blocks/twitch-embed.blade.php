36dd95c245c2da55a23bf68ed445bef3160829ae
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-07-18 00:40:57 -0400

    Refactor Twitch embed to improve integration flexibility.
    Added theme settings and dynamic host parsing for better configurability. Modified Twitch embed URLs to use consistent parameters and updated autoplay behavior with muted default. Enhanced maintainability and adherence to best practices.

--- resources/views/components/filament-fabricator/page-blocks/twitch-embed.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/twitch-embed.blade.php
@@ -1,5 +1,6 @@
 @php
-    $settings = app(\App\Settings\TwitchSettings::class);
+    use App\Settings\LookFeelSettings;use App\Settings\TwitchSettings;use function PHPUnit\Framework\isFalse;$settings = app(TwitchSettings::class);
+    $themeSettings = app(LookFeelSettings::class);
 @endphp
 
 @if (! $settings->enable_integration)
@@ -16,7 +17,11 @@
         $channel           = $channel ?? ($data['channel'] ?? $settings->channel_name);
         $chat              = $chat ?? ($data['chat'] ?? false);
         $autoplay          = $autoplay ?? ($data['autoplay'] ?? false);
+        $muted             = false;
+        if($autoplay) { $muted = true; };
         $horizontal_layout = $horizontal_layout ?? ($data['horizontal_layout'] ?? false);
+        $theme             = $themeSettings->theme ?? 'light';
+        $host              = parse_url(config('app.url'), PHP_URL_HOST);
     @endphp
 
     <div class="px-4 py-4 md:py-8">
@@ -24,23 +29,17 @@
             <div style="display: flex; flex-direction: {{ $horizontal_layout ? 'row' : 'column' }};">
                 <div class="aspect-w-16 aspect-h-9">
                     <iframe
-                        src="https://embed.twitch.tv/?allowfullscreen=true
-                             &channel={{ $channel }}
-                             &autoplay={{ $autoplay ? 'true' : 'false' }}
-                             &layout=video
-                             &parent={{ request()->getHost() }}"
-                        width="100%" height="600"
-                        allow="autoplay; fullscreen"
-                        scrolling="no"
-                        sandbox="allow-modals allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-storage-access-by-user-activation"
-                        id="stream_embed">
+                        src="https://player.twitch.tv/?channel={{ $channel }}&parent={{ $host }}&muted={{ $muted }}$autoplay={{ $autoplay }}"
+                        height="auto"
+                        width="100%"
+                        allowfullscreen>
                     </iframe>
                 </div>
 
                 @if ($chat)
                     <div class="{{ $horizontal_layout ? 'w-full md:w-1/3' : '' }} mt-4 md:mt-0">
                         <iframe
-                            src="https://www.twitch.tv/embed/{{ $channel }}/chat?parent={{ request()->getHost() }}"
+                            src="https://www.twitch.tv/embed/{{ $channel }}/chat?parent={{ $host }}"
                             width="100%" height="500"
                             id="chat_embed">
                         </iframe>
