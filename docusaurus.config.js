9d51787b724eb23dd9997e5ad3492a9c7ba35733
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-07-27 23:08:52 -0400

    Remove editUrl fields from Docusaurus config
    Simplified the configuration by removing unused `editUrl` fields in the docs and blog sections. This eliminates unnecessary placeholder links and ensures a cleaner configuration setup.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,10 +49,6 @@ const config = {
             ({
                 docs: {
                     sidebarPath: './sidebars.js',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 blog: {
                     showReadingTime: true,
@@ -60,11 +56,6 @@ const config = {
                         type: ['rss', 'atom'],
                         xslt: true,
                     },
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                    // Useful options to enforce blogging best practices
                     onInlineTags: 'warn',
                     onInlineAuthors: 'warn',
                     onUntruncatedBlogPosts: 'warn',
