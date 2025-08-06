ebed3aad1ecd915857f4878381b2b216c17f3c7f
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-08-06 12:02:43 -0400

    Handle nullable timestamps in ProductResource.
    Updated the `created_at` and `updated_at` fields to safely handle null values using the null safe operator. This change prevents potential errors when these fields are not set.

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -38,8 +38,8 @@ class ProductResource extends JsonResource
                 ShopHelper::getShopSlug()
             ),
             'images' => $images,
-            'created_at' => $this->created_at->toIso8601String(),
-            'updated_at' => $this->updated_at->toIso8601String(),
+            'created_at' => $this->created_at?->toIso8601String(),
+            'updated_at' => $this->updated_at?->toIso8601String(),
         ];
     }
 }
